24e682a494783a9b7aa8c3fbd34e2d549557da69
Ørjan Vøllestad <orjanv@gmail.com>
2017-03-07 11:05:05 +0100

    first commit

---
+++ iphaiku.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+from pyhipku import encode
+import re
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def getip():
+    if request.method == "GET":
+        try:
+            IPADDRESS = request.headers.get('X-Forwarded-For', request.remote_addr)
+            haiku = re.sub('\n', '<br>', (encode(IPADDRESS)))
+            return render_template('index.html', haiku=haiku, ip=IPADDRESS)
+        except:
+            return("didn't work")
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', port=5000)
